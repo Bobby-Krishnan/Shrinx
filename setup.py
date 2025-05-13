241b79f18473cc229595c7664fa94af0657194e3
Bobby-Krishnan <Abhishek.v.krishnan@gmail.com>
2025-05-13 12:36:04 -0500

    pip

--- setup.py
+++ setup.py
@@ -19,3 +19,14 @@ setup(
     },
     python_requires=">=3.8"
 )
+
+def main():
+    parser = argparse.ArgumentParser(description="Token Compression CLI Tool")
+    parser.add_argument("input", help="Path to input JSON file")
+    parser.add_argument("--output", help="Path to output compressed JSON file", required=True)
+    parser.add_argument("--use_wizardlm", action="store_true", help="Use WizardLM for compression")
+    parser.add_argument("--limit", type=int, help="Limit number of lines processed (optional)")
+    
+    args = parser.parse_args()
+    run_compression(args.input, args.output, args.use_wizardlm)
+
